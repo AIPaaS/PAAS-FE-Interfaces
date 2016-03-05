ef007ecf905ae7e306813f9eb2519107bb95150b
bixy <bixy@bogon>
2016-03-05 15:53:02 +0800

    增加控制器

--- src/main/java/com/aic/paas/task/mvc/dep/PcAppImageMvc.java
+++ src/main/java/com/aic/paas/task/mvc/dep/PcAppImageMvc.java
@@ -10,40 +10,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.aic.paas.task.peer.dep.PcAppImagePeer;
 import com.binary.core.util.BinaryUtils;
 
-
-
 @Controller
 @RequestMapping("/dep/appimage")
 public class PcAppImageMvc {
-	
-	
+
 	@Autowired
 	PcAppImagePeer appImagePeer;
-	
-	
-		
-	
+
 	/**
 	 * 开始部署
+	 * 
 	 * @param appId
 	 * @param appVnoId
 	 */
 	@RequestMapping("/startDeploy")
 	public void startDeploy(HttpServletRequest request, HttpServletResponse response, Long appId, Long appVnoId) {
-		
+
 		System.out.println("===================== appId : " + appId);
 		System.out.println("===================== appVnoId : " + appVnoId);
-		
+
 		BinaryUtils.checkEmpty(appId, "appId");
 		BinaryUtils.checkEmpty(appVnoId, "appVnoId");
 		appImagePeer.startDeploy(appId, appVnoId);
 	}
-	
-	
-	
-	
+
 	/**
 	 * 重新部署
+	 * 
 	 * @param appId
 	 * @param appVnoId
 	 */
@@ -53,12 +46,10 @@ public class PcAppImageMvc {
 		BinaryUtils.checkEmpty(appVnoId, "appVnoId");
 		appImagePeer.reDeploy(appId, appVnoId);
 	}
-	
-	
-	
-	
+
 	/**
 	 * 停止部署
+	 * 
 	 * @param appId
 	 * @param appVnoId
 	 */
@@ -68,10 +59,31 @@ public class PcAppImageMvc {
 		BinaryUtils.checkEmpty(appVnoId, "appVnoId");
 		appImagePeer.destroyDeploy(appId, appVnoId);
 	}
-	
-	
-	
-	
-	
+
+	/**
+	 * 开始运行app
+	 * 
+	 * @param appId
+	 * @param appVnoId
+	 */
+	@RequestMapping("/startApp")
+	public void start(HttpServletRequest request, HttpServletResponse response, Long appId, Long appVnoId) {
+		BinaryUtils.checkEmpty(appId, "appId");
+		BinaryUtils.checkEmpty(appVnoId, "appVnoId");
+		appImagePeer.startApp(appId, appVnoId);
+	}
+
+	/**
+	 * 暂停运行app
+	 * 
+	 * @param appId
+	 * @param appVnoId
+	 */
+	@RequestMapping("/pauseApp")
+	public void pause(HttpServletRequest request, HttpServletResponse response, Long appId, Long appVnoId) {
+		BinaryUtils.checkEmpty(appId, "appId");
+		BinaryUtils.checkEmpty(appVnoId, "appVnoId");
+		appImagePeer.pauseApp(appId, appVnoId);
+	}
 
 }
