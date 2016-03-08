6bbbb52da9ba584ebedd9a2840fe0785fe218c4e
bixy <bixy@bogon>
2016-03-08 14:56:34 +0800

    修改开始暂停应用获取应用版本信息

--- src/main/java/com/aic/paas/task/callback/impl/DeployAppCallbackSvcImpl.java
+++ src/main/java/com/aic/paas/task/callback/impl/DeployAppCallbackSvcImpl.java
@@ -1,12 +1,14 @@
 package com.aic.paas.task.callback.impl;
 
+import com.aic.paas.task.callback.CallBackReq;
 import com.aic.paas.task.callback.DeployAppCallbackSvc;
+import com.binary.json.JSON;
 
 public class DeployAppCallbackSvcImpl implements DeployAppCallbackSvc {
 
 	@Override
 	public void callDeployServiceFinished(String param) {
-		
+		CallBackReq callBackReq = JSON.toObject(param, CallBackReq.class);
 	}
 
 }
--- src/main/java/com/aic/paas/task/peer/dep/PcAppImagePeer.java
+++ src/main/java/com/aic/paas/task/peer/dep/PcAppImagePeer.java
@@ -5,72 +5,59 @@ import java.util.List;
 import com.aic.paas.task.bean.dep.AppImageSettings;
 
 public interface PcAppImagePeer {
-	
-	
-	
-	
+
 	/**
 	 * 获取镜像所有配置信息
+	 * 
 	 * @param appImageId
 	 * @return
 	 */
 	public AppImageSettings getAppImageSettings(Long appImageId);
-	
-	
-	
-	
+
 	/**
 	 * 获取应用下所有容器配置
+	 * 
 	 * @param appId
 	 * @param appVnoId
 	 * @return
 	 */
 	public List<AppImageSettings> getAppImageSettingsList(Long appId, Long appVnoId);
-	
-	
-	
-	
-	
+
 	/**
 	 * 开始部署
+	 * 
 	 * @param appId
 	 * @param appVnoId
 	 */
 	public String startDeploy(Long appId, Long appVnoId);
-	
-	
-	
+
 	/**
 	 * 重新部署
+	 * 
 	 * @param appId
 	 * @param appVnoId
 	 */
 	public String reDeploy(Long appId, Long appVnoId);
-	
-	
-	
-	
+
 	/**
 	 * 停止部署
+	 * 
 	 * @param appId
-	 * @param appVnoId
 	 */
-	public String destroyDeploy(Long appId, Long appVnoId);
-	
+	public String destroyDeploy(Long appId);
+
 	/**
 	 * 开始运行应用
+	 * 
 	 * @param appId
-	 * @param appVnoId
 	 */
-	public String startApp(Long appId, Long appVnoId);
-	
+	public String startApp(Long appId);
+
 	/**
 	 * 暂停运行应用
+	 * 
 	 * @param appId
-	 * @param appVnoId
 	 */
-	public String pauseApp(Long appId, Long appVnoId);
-	
-	
+	public String pauseApp(Long appId);
 
 }
