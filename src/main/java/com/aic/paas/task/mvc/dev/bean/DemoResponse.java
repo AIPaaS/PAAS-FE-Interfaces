32ce196e62829b8d7ebb2fcbfef8a47c2455ecd4
mapl-pisces <mapl@asiainfo.com>
2016-03-11 17:42:10 +0800

    demo

---
+++ src/main/java/com/aic/paas/task/mvc/dev/bean/DemoResponse.java
@@ -0,0 +1,32 @@
+package com.aic.paas.task.mvc.dev.bean;
+
+import com.aic.paas.task.mvc.dev.bean.base.BaseResponse;
+
+public class DemoResponse extends BaseResponse {
+	
+	 private String   build_id;
+	 private String    created_at;
+	 private String    status;
+	public String getBuild_id() {
+		return build_id;
+	}
+	public void setBuild_id(String build_id) {
+		this.build_id = build_id;
+	}
+	public String getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	 
+	 
+
+
+}
