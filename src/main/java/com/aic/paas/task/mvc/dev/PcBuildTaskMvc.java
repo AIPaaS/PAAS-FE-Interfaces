bf575df6ff60afc925a0abad689d5a8471b7eb3a
oriiro <mkoh2c03@163.com>
2016-03-14 23:15:59 +0800

    testqueryRecord

--- src/main/java/com/aic/paas/task/mvc/dev/PcBuildTaskMvc.java
+++ src/main/java/com/aic/paas/task/mvc/dev/PcBuildTaskMvc.java
@@ -1,15 +1,19 @@
 package com.aic.paas.task.mvc.dev;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.aic.paas.task.mvc.dev.bean.DemoResponse;
+import com.binary.core.util.BinaryUtils;
 import com.binary.json.JSON;
+import com.binary.json.JSONObject;
 
 @Controller
 @RequestMapping("/dev/buildTaskMvc")
@@ -59,6 +63,32 @@ public class PcBuildTaskMvc {
 		deresp.setNamespace("delete");
 		return JSON.toString(deresp);		
 	}*/
+	@RequestMapping("/queryTaskRecord")
+	@ResponseBody
+	public String queryTaskRecord(@RequestBody String param) {
+		BinaryUtils.checkEmpty(param, "param");
+		JSONObject data = new JSONObject(param);
+		String repo_name = data.getString("repo_name");
+		String namespace = data.getString("namespace");
+		String build_id = data.getString("build_id");
+		JSONObject resultInfo=new JSONObject();
+		resultInfo.put("namespace", "asiainfo");
+		resultInfo.put("repo_name", repo_name);
+		resultInfo.put("build_id", build_id);
+		resultInfo.put("building", false);
+		resultInfo.put("duration", 8);
+		resultInfo.put("started_at", "2015-09-02T08:17:42.581Z");
+		resultInfo.put("status", "success");
+		resultInfo.put("stdout", "Started by user admin "
+				+ "Building in workspace /var/jenkins_home/workspace/test\r\n"
+				+ "[test] $ /bin/sh -xe /tmp/hudson7038042263921764692.sh\r\n"
+				+ " echo /var/jenkins_home/workspace/test\r\n"
+				+ "/var/jenkins_home/workspace/test\r\n"
+				+ "Notifying upstream projects of job completion\r\n"
+				+ "Finished: SUCCESS");
+		return resultInfo.toString();
+
+	}
 	
 	
 	
