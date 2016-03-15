5098bb36c34f81e555daff479e9f637a27d8b1d0
renfeng <renfeng@renfeng-PC>
2016-03-15 19:58:25 +0800

    功能修改

--- src/main/java/com/aic/paas/task/mvc/dev/PcBuildDefMvc.java
+++ src/main/java/com/aic/paas/task/mvc/dev/PcBuildDefMvc.java
@@ -1,47 +1,31 @@
 package com.aic.paas.task.mvc.dev;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.aic.paas.task.util.http.HttpClientUtil;
-import com.binary.json.JSON;
+import com.aic.paas.task.peer.dev.PcBuildDefPeer;
 
 @Controller
 @RequestMapping("/dev/buildDefMvc")
 public class PcBuildDefMvc {
 	
 	static final Logger logger = LoggerFactory.getLogger(PcBuildDefMvc.class);
-	
+	@Autowired
+	private PcBuildDefPeer pcBuildDefPeer;
 
-	@SuppressWarnings("unchecked")
 	@RequestMapping(value="buildDefApi")
 	public @ResponseBody String buildDefApi(@RequestBody String param) {	
-		String result = HttpClientUtil.sendPostRequest("http://10.1.245.139:8088/v1/repositories", param);
-		if(result!=null&&!"".equals(result)){
-			Map<String,String> map = JSON.toObject(result, Map.class);
-			return map.get("status");
-		}else{
-			return "error";
-		}
+		return pcBuildDefPeer.buildDefApi(param);
 	}
-	@SuppressWarnings("unchecked")
 	@RequestMapping(value="updateBuildDefApi")
 	public @ResponseBody String updateBuildDefApi(@RequestBody String param) {	
-		String result = HttpClientUtil.sendPutRequest("http://10.1.245.139:8088/v1/repositories", param);
-		if(result!=null&&!"".equals(result)){
-			Map<String,String> map = JSON.toObject(result, Map.class);
-			return map.get("status");
-		}else{
-			return "error";
-		}
+		return pcBuildDefPeer.updateBuildDefApi(param);
 	}
-	
 	
 
 }
