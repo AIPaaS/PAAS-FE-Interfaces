8bf6a6b2fae373f4729e78bc8f112afc17d50827
zhaolijing1990 <452274335@qq.com>
2016-03-21 17:45:51 +0800

    task中添加logger日志

--- src/main/java/com/aic/paas/task/dev/mvc/PcBuildTaskMvc.java
+++ src/main/java/com/aic/paas/task/dev/mvc/PcBuildTaskMvc.java
@@ -71,6 +71,7 @@ public class PcBuildTaskMvc {
 	@RequestMapping(value="updateBuildTaskByCallBack")
 	@ResponseBody
 	public String updateBuildTaskByCallBack(@RequestBody String param){
+		logger.info("paas-task:PcBuildTaskMvc:updateBuildTaskByCallBack:param", param);
 		System.out.println("param =========================="+param);
 		PcBuildTaskCallBack pbtc = new PcBuildTaskCallBack();
 		pbtc = JSON.toObject(param, PcBuildTaskCallBack.class);
@@ -79,8 +80,10 @@ public class PcBuildTaskMvc {
 		String result ="";
 		try {
 			result = buildTaskPeer.updateBuildTaskByCallBack(pbtc);
+			logger.info("paas-task:PcBuildTaskMvc:updateBuildTaskByCallBack:result", result);
 		} catch (Exception e) {
 			result = "error";
+			logger.info("paas-task:PcBuildTaskMvc:updateBuildTaskByCallBack:result", result);
 		}
 		
 		return result;
