1e1e1cb919862750c992f1ddade9db704281f771
zhangqiang7 <zhangqiang7@aisainfo.com>
2016-03-23 19:39:16 +0800

    增加历史记录信息数据

--- src/main/java/com/aic/paas/task/dep/peer/impl/PcAppImagePeerImpl.java
+++ src/main/java/com/aic/paas/task/dep/peer/impl/PcAppImagePeerImpl.java
@@ -139,8 +139,13 @@ public class PcAppImagePeerImpl implements PcAppImagePeer {
 				BeanUtils.copyProperties(pcAppDepHistory, pcApp);
 				BeanUtils.copyProperties(pcAppDepHistory, setting);
 				pcAppDepHistory.setTaskId(resp.getReqId().longValue());
-				pcAppDepHistory.setContainerName(setting.getAppImage().getContainerFullName());
+				pcAppDepHistory.setContainerName(setting.getAppImage().getContainerFullName().toLowerCase());
 				pcAppDepHistory.setId(null);
+				pcAppDepHistory.setAppVersionNo(pcApp.getVersionNo());
+				pcAppDepHistory.setNetZoneId(setting.getAppImage().getNetZoneId());
+				pcAppDepHistory.setImageName(setting.getImage().getImageName());
+				pcAppDepHistory.setImageFullName(setting.getAppImage().getContainerFullName());
+				pcAppDepHistory.setContainerFullName(setting.getAppImage().getContainerFullName());
 				pcAppDepHistorySvc.saveOrUpdate(pcAppDepHistory);
 			} catch (Exception e) {
 				logger.error("", e);
