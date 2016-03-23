7c8899d0fadfcede411359f278bfb7c911026435
yuanman <yuanman@asiainfo.com>
2016-03-23 17:01:26 +0800

    重定义包路径

--- src/main/java/com/aic/paas/task/res/peer/impl/PcResManagePeerImpl.java
+++ src/main/java/com/aic/paas/task/res/peer/impl/PcResManagePeerImpl.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.aic.paas.task.res.peer.PcResManagePeer;
-import com.aic.paas.task.rest.res.IResCenterServiceManager;
+import com.aic.paas.task.res.rest.IResCenterServiceManager;
 
 public class PcResManagePeerImpl implements PcResManagePeer {
 	static final Logger logger = LoggerFactory.getLogger(PcResManagePeerImpl.class);
